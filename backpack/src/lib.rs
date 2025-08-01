1889186737754d3f5e3623bb6b47c686958d7b7d
Yonas <yonas.y@gmail.com>
2025-08-01 14:43:49 -0400

    feat: Add surf2 custom HTTP client.

--- backpack/src/lib.rs
+++ backpack/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod surf2;
+
 /// Multiplies two integers
 pub fn multiply(a: i32, b: i32) -> i32 {
     a * b
@@ -39,6 +41,20 @@ mod test {
                 it "awaits" {
                     assert_eq!(four, is_4_task.await)
                 }
+            }
+        }
+
+        describe "surf2" {
+            use super::*;
+            use crate::surf2::{get, Response};
+
+            test "HTTP get request" {
+                let res = surf2::get("https://httpbin.org/get").unwrap();
+
+                assert!(res.body().len() > 0);
+                assert!(res.headers().keys().len() > 0);
+                assert_eq!(res.headers()["content-type"], "application/json");
+                assert_eq!(res.status(), 0);
             }
         }
     }
---
+++ backpack/src/surf2.rs
@@ -0,0 +1,81 @@
+#[derive(Default)]
+pub struct Req {
+    headers: std::collections::HashMap<String, String>,
+}
+
+pub struct Resp {
+    body: String,
+    headers: std::collections::HashMap<String, String>,
+    status: u16,
+}
+
+pub trait Request {
+    fn header(&mut self, key: &str, value: &str);
+    fn get(&self, url: &str) -> Result<Resp, Box<dyn std::error::Error>>;
+}
+
+pub trait Response {
+    fn body(&self) -> String;
+    fn headers(&self) -> std::collections::HashMap<String, String>;
+    fn status(&self) -> u16;
+}
+
+impl Request for Req {
+    fn header(&mut self, key: &str, value: &str) {
+        self.headers.insert(key.to_string(), value.to_string());
+    }
+
+    fn get(&self, url: &str) -> Result<Resp, Box<dyn std::error::Error>> {
+        /*
+        let mut res = ureq::get(url);
+
+        // Set request headers
+        for (key, value) in self.headers.iter() {
+            res.header(key.as_str(), value.as_str());
+        }
+
+        // Make HTTP call
+        let mut body = res.call()?;
+        */
+        let mut res = ureq::get(url).call()?;
+
+        // Extact response headers
+        let mut headers = std::collections::HashMap::<String, String>::new();
+        for (key, value) in res.headers().iter() {
+            headers.insert(
+                key.to_string().clone(),
+                String::from(value.to_str().unwrap()),
+            );
+        }
+
+        // Create response object
+        let resp = Resp {
+            body: res.body_mut().read_to_string()?,
+            headers: headers,
+            status: 0,
+        };
+
+        Ok(resp)
+    }
+}
+
+impl Response for Resp {
+    fn body(&self) -> String {
+        self.body.clone()
+    }
+
+    fn headers(&self) -> std::collections::HashMap<String, String> {
+        self.headers.clone()
+    }
+
+    fn status(&self) -> u16 {
+        self.status.clone()
+    }
+}
+
+pub fn get(url: &str) -> Result<Resp, Box<dyn std::error::Error>> {
+    let req = Req {
+        headers: Default::default(),
+    };
+    req.get(url)
+}
